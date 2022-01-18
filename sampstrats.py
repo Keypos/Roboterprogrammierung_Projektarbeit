ad4f2d31db3b66390df5e9c2a382b5f688a269d5
thecherry94 <philipp.kirsch94@googlemail.com>
2022-01-18 13:22:14 +0100

    Update sampstrats.py

--- sampstrats.py
+++ sampstrats.py
@@ -45,10 +45,10 @@ class GaussianPRM(IPPRMBase.PRMBase):
         while i < numNodes:
             
             # Generate a 'randomly chosen, free configuration'
-            pos = self._sample()
+            pos = Gaussian_sampling(self._collisionChecker)
             
             # Find set of candidates to connect to sorted by distance
-            result = self._nearestNeighboursXX(pos, self._graph, radius)
+            result = self._nearestNeighboursX(pos, self._graph, radius)
             
             # check connection
             self._graph.add_node(i, pos=pos)
@@ -58,51 +58,6 @@ class GaussianPRM(IPPRMBase.PRMBase):
                         self._graph.add_edge(i,data[1][0])
                         
             i+=1
-    
-    def _sample(self):
-        limits = self._collChecker.getEnvironmentLimits()        
-        pos = [random.uniform(limit[0],limit[1]) for limit in limits]
-        
-        
-        #get a colliding point
-        for t in range(0,10):
-            while not self._collChecker.pointInCollision(pos):
-                pos = [random.uniform(limit[0],limit[1]) for limit in limits]
-            
-            #store the x and y value of the colliding point
-            pos_x=pos[0]
-            pos_y=pos[1]
-        
-            #find a non colliding point in a given distance to point a 
-            for i in range(0,50):
-            #get a distance over a gaussian distribution   
-                me,sigma = 0,1 #Mean value of the gaussian distribution, standard deviation 
-                d=np.random.normal(me,sigma)
-                angle_list = [0,2*math.pi]
-                for n in range(8):
-                    #alpha=random.uniform(0,360)*(180/math.pi) #get an random angle in ra
-                    templist = copy.deepcopy(angle_list)
-                    new_angles_list =[]
-                    verschoben = 1 #ja es gibt was hier zu sehen
-                    for idx in range(len(templist)-1):
-                        new_angle = (templist[idx]+templist[idx+1])/2
-                        new_angles_list.append(new_angle)
-                        angle_list.insert(idx+verschoben,new_angle)
-                        verschoben +=1
-                    for alpha in new_angles_list:    
-                        pos2_x= d*math.cos(alpha)+pos_x
-                        pos2_y= d*math.sin(alpha)+pos_y
-                        pos2=[pos2_x,pos2_y]
-                        if not self._collChecker.pointInCollision(pos2): #return point when collision free
-                            return pos2
-        
-        return False
-        #if no point found with gaussian pick a random collison free point
-        """"
-        while collChecker.pointInCollision(pos):
-            pos = [random.uniform(limit[0],limit[1]) for limit in limits]
-        return pos
-        """
     
     def planPath(self, start, goal, config):
         # find nearest, collision-free connection between node on graph and start
